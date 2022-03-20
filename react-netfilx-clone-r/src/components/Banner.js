dff616a75acb2df584b59b51926ddef626aaac3d
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-03-20 22:54:59 +0900

    feat : 6-2

--- react-netfilx-clone-r/src/components/Banner.js
+++ react-netfilx-clone-r/src/components/Banner.js
@@ -64,13 +64,38 @@ if(!isClicked){
 }else{
     return(
         <Container>
-            <homeContainer>
-            clicked
-            </homeContainer>
+            <HomeContainer>
+            <Iframe 
+                width="640"
+                height="360"
+                src={`https://www.youtube.com/embed/${movie.videos.results[0].key}?controls=&autoplay=1&loop=1&mute=1&playlist=${movie.videos.results[0].key}`}
+                title="YouTube video player" 
+                frameborder="0" 
+                allow=" autoplay; fullscreen" allowfullscreen>
+            </Iframe>
+            </HomeContainer>
         </Container>
     )
 }
 }
+// 컴포넌트는 항상 대문자로 시작해야한다. 
+const Iframe = styled.iframe` 
+    width : 100%;
+    height : 100%; 
+    z-index : -1;
+    
+    border : none;
+
+    &::after{
+        content:"";
+        position: absolute;
+        top : 0;
+        left : 0;
+        width : 100%;
+        height : 100%; 
+    }
+
+`
 
 const Container = styled.div`
     display: flex;
@@ -81,7 +106,7 @@ const Container = styled.div`
     height : 100vh;
 `
 
-const homeContainer = styled.div`
+const HomeContainer = styled.div`
     width : 100%;
     height : 100%;
 `
