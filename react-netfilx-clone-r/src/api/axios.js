36d2b9f4de1688b7366ffdad80cf09e7eda99053
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-04-04 13:31:54 +0900

    initial

--- react-netfilx-clone-r/src/api/axios.js
+++ react-netfilx-clone-r/src/api/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
     baseURL: "https://api.themoviedb.org/3/",
     params:{
-        api_key:"b1e1a6c6195bd0f36adddb589e1bde7d",
+        api_key:process.env.REACT_APP_MOVIE_DB_API_KEY ,
         language:"ko-KR",
     },
 });
