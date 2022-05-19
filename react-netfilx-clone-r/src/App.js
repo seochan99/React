1c9b58a4eb100c34c5d37f6195fba1738dc58433
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-05-19 19:24:40 +0900

    Update App.js

--- react-netfilx-clone-r/src/App.js
+++ react-netfilx-clone-r/src/App.js
@@ -1,40 +1,35 @@
-import './App.css';
+import "./App.css";
 import Nav from "./components/Nav";
-import Footer from './components/Footer';
-import { Outlet, Route, Routes } from 'react-router-dom';
+import Footer from "./components/Footer";
+import { Outlet, Route, Routes } from "react-router-dom";
 
-import DetailPage  from './pages/DetailPage';
-import MainPage from './pages/MainPage';
-import SearchPage from './pages/SearchPage';
+import DetailPage from "./pages/DetailPage";
+import MainPage from "./pages/MainPage";
+import SearchPage from "./pages/SearchPage";
 
-const Layout = () =>{
-  return(
-    <div>
-      <Nav/>
+const Layout = () => {
+    return (
+        <div>
+            <Nav />
 
-      <Outlet/>
+            <Outlet />
 
-      <Footer/>
-
-    </div>
-  )
-}
-
-
+            <Footer />
+        </div>
+    );
+};
 function App() {
-  return (
-    <div className="App">
-      <Routes>
-        <Route path='/' element={<Layout/>}>
-          <Route index element={<MainPage/>} />
-          <Route path=":movieId" element={<DetailPage/>} />
-          <Route path="search" element={<SearchPage/>} />
-
-        </Route>
-      </Routes>
-
-    </div>
-  );
+    return (
+        <div className="App">
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<MainPage />} />
+                    <Route path=":movieId" element={<DetailPage />} />
+                    <Route path="search" element={<SearchPage />} />
+                </Route>
+            </Routes>
+        </div>
+    );
 }
 
 export default App;
