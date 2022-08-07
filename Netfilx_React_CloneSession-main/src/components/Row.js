3f94450266f50a163e6c5484ca5e178343bf5b7c
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-08-07 23:31:37 +0900

    Session7:Likelion

--- Netfilx_React_CloneSession-main/src/components/Row.js
+++ Netfilx_React_CloneSession-main/src/components/Row.js
@@ -1,9 +1,19 @@
+import React, { useEffect, useState } from "react";
 import axios from "../api/axios";
-import { useEffect, useState } from "react";
+import MovieModal from "./MovieModal";
 import "./Row.css";
-import MovieModal from "./MovieModal/index";
 
-function Row({ title, id, fetchUrl, isLargeRow }) {
+import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
+
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "swiper/css/scrollbar";
+
+export default function Row({ isLargeRow, title, id, fetchUrl }) {
     const [movies, setMovies] = useState([]);
     const [modalOpen, setModalOpen] = useState(false);
     const [movieSelected, setMovieSelected] = useState({});
@@ -18,47 +28,87 @@ function Row({ title, id, fetchUrl, isLargeRow }) {
     };
 
     const handleClick = (movie) => {
-        setModalOpen(true);
+        setModalOpen(true); //모달 오픈이 트루
         setMovieSelected(movie);
     };
 
-    const BASE_URL = "https://image.tmdb.org/t/p/original/";
     return (
         <section className="row">
             <h2>{title}</h2>
-            {/* 슬라이더 */}
-            <div className="slider">
-                <div className="slider__arrow-left">
-                    <span className="arrow">{"<"}</span>
-                </div>
-                {/* 영화 여러 개를 key 값을 이용해 반복문 돌리기 */}
+            <Swiper
+                // install Swiper modules
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                navigation
+                pagination={{ clickable: true }}
+                loop={true} //끝에 갔을때 돌아오기
+                breakpoints={{
+                    //크기별로 다르게 주기
+                    1378: {
+                        slidesPerView: 6,
+                        slidesPerGroup: 6,
+                    },
+                    998: {
+                        slidesPerView: 5,
+                        slidesPerGroup: 5,
+                    },
+                    625: {
+                        slidesPerView: 4,
+                        slidesPerGroup: 4,
+                    },
+                    0: {
+                        slidesPerView: 3,
+                        slidesPerGroup: 3,
+                    },
+                }}
+                onSlideChange={() => console.log("slide change")}
+            >
+                {/* <div
+                className='slider__arrow-left'               
+                onClick={()=>{ //스크롤 기능(완)
+                        document.getElementById(id).scrollLeft -= window.innerWidth - 80;
+                    }}>
+                <span
+                    className='arrow' 
+
+                    >
+                        {"<"}
+                </span>
+            </div> */}
                 <div id={id} className="row__posters">
                     {movies.map((movie) => (
-                        <img
-                            key={movie.id}
-                            className={`row__poster ${
-                                isLargeRow && "row__posterLarge"
-                            }`}
-                            src={`${BASE_URL}${
-                                isLargeRow
-                                    ? movie.poster_path
-                                    : movie.backdrop_path
-                            }`}
-                            loading="lazy"
-                            alt={movie.name}
-                            onClick={() => handleClick(movie)}
-                        />
+                        <SwiperSlide>
+                            <img
+                                key={movie.id}
+                                className={`row__poster ${
+                                    isLargeRow && "row__posterLarge"
+                                }`}
+                                src={`https://image.tmdb.org/t/p/original/${
+                                    isLargeRow
+                                        ? movie.poster_path
+                                        : movie.backdrop_path
+                                }`}
+                                loading="lazy"
+                                alt={movie.name}
+                                onClick={() => handleClick(movie)}
+                            />
+                        </SwiperSlide>
                     ))}
                 </div>
-                <div className="slider__arrow-right">
-                    <span className="arrow">{">"}</span>
-                </div>
-            </div>
-            {modalOpen && (
-                <MovieModal {...movieSelected} setModalOpen={setModalOpen} />
+                {/* <div 
+                className='slider__arrow-right'
+                onClick={()=>{ //스크롤 기능(오)
+                    document.getElementById(id).scrollLeft += window.innerWidth - 80;
+                }}>
+                <span 
+                className='arrow'
+                
+                >{">"}</span>
+            </div> */}
+            </Swiper>
+            {modalOpen && ( //모달 오픈시
+                <MovieModal {...movieSelected} setModalOpen={setModalOpen} /> //컴포넌트 가져오기
             )}
         </section>
     );
 }
-
-export default Row;
+//스와이프모르게따
--- react-netfilx-clone-r/src/components/Nav.js
+++ react-netfilx-clone-r/src/components/Nav.js
@@ -1,51 +1,53 @@
-import React, { useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import "./Nav.css"
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "./Nav.css";
 
 export default function Nav() {
-
     const [show, setShow] = useState(false);
+
     const [searchValue, setsearchValue] = useState("");
     const navigate = useNavigate();
 
     useEffect(() => {
-        window.addEventListener("scroll",()=>{
-            if(window.scrollY > 50){
+        window.addEventListener("scroll", () => {
+            if (window.scrollY > 50) {
                 setShow(true);
-            }else{
+            } else {
                 setShow(false);
             }
-        })
-        return () => {    
-            window.removeEventListener("scroll",()=>{});
+        });
+        return () => {
+            window.removeEventListener("scroll", () => {});
         };
-    }, [])
-    
+    }, []);
+
     const handleChange = (e) => {
-        setsearchValue(e.target.value); //바로 검색가능하다. 
-        navigate(`/search?q=${e.target.value}`)
-        
-
+        setsearchValue(e.target.value); //바로 검색가능하다.
+        navigate(`/search?q=${e.target.value}`);
     };
-    
 
+    return (
+        <nav className={`nav ${show && "nav_black"}`}>
+            <img
+                alt="Netflix logo"
+                src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
+                className="nav__logo"
+                onClick={() => window.location.reload()}
+            />
 
+            <input
+                value={searchValue}
+                onChange={handleChange}
+                className="nav__input"
+                type="text"
+                placeholder="영화제목을 입력해주세요."
+            />
 
-    return <nav className={`nav ${show && "nav_black" }`}>
-        <img
-        alt="Netflix logo"
-        src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2880px-Netflix_2015_logo.svg.png"
-        className='nav__logo'
-        onClick={()=>window.location.reload()}
-        />
-
-        <input value={searchValue} onChange={handleChange} className="nav__input" type="text" placeholder='영화제목을 입력해주세요.'></input>
-
-        <img
-        alt="User logged"
-        src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
-        className='nav__avater'
-        />
-    </nav>
-
+            <img
+                alt="User logged"
+                src="https://ih0.redbubble.net/image.618427277.3222/flat,1000x1000,075,f.u2.jpg"
+                className="nav__avater"
+            />
+        </nav>
+    );
 }
