c796c86f7fda728a70389e1bb642e16553ebd561
HuiChan Seo <78739194+seochan99@users.noreply.github.com>
2022-08-11 23:53:55 +0900

    Update index.js

--- react-netfilx-clone-r/src/pages/MainPage/index.js
+++ react-netfilx-clone-r/src/pages/MainPage/index.js
@@ -1,40 +1,36 @@
-import React from 'react'
-import Banner from '../../components/Banner'
-import Row from '../../components/Row'
-import requests from '../../api/requests'
+import React from "react";
+import Banner from "../../components/Banner";
+import Row from "../../components/Row";
+import requests from "../../api/requests";
 
 export default function MainPage() {
-  return (
-    <div>
-              <Banner/>
+    return (
+        <div>
+            <Banner />
 
-<Row 
-  title="NETFLIX ORIGINALS"
-  id="NO"
-  fetchUrl={requests.fetchNetflixOriginals}
-  isLargeRow
-/>
+            <Row
+                title="NETFLIX ORIGINALS"
+                id="NO"
+                fetchUrl={requests.fetchNetflixOriginals}
+                isLargeRow
+            />
 
-<Row
-  title="Trending Now"
-  id = "TN"
-  fetchUrl={requests.fetchTrending}
-/>
-<Row
-  title="Top Rated"
-  id = "TR"
-  fetchUrl={requests.fetchTopRated}
-/>
-<Row
-  title="Action Movies"
-  id = "AM"
-  fetchUrl={requests.fetchActionMovies}
-/>
-<Row
-  title="Comnedy Movies"
-  id = "CM"
-  fetchUrl={requests.fetchComedyMovies}
-/>
-    </div>
-  )
-};
+            <Row
+                title="Trending Now"
+                id="TN"
+                fetchUrl={requests.fetchTrending}
+            />
+            <Row title="Top Rated" id="TR" fetchUrl={requests.fetchTopRated} />
+            <Row
+                title="Action Movies"
+                id="AM"
+                fetchUrl={requests.fetchActionMovies}
+            />
+            <Row
+                title="Comnedy Movies"
+                id="CM"
+                fetchUrl={requests.fetchComedyMovies}
+            />
+        </div>
+    );
+}
